0de8fa54b6e769b996182e0e2508965b0bd591e7
Patel Yashu <patelyashu1724@gmail.com>
2024-04-29 15:46:43 +0530

    Initial commit

---
+++ lib/Buttons/button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class buttonApp extends StatefulWidget {
+  const buttonApp({super.key});
+
+  @override
+  State<buttonApp> createState() => _buttonAppState();
+}
+
+class _buttonAppState extends State<buttonApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xffF8F3F9),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Elevated',
+                    style: TextStyle(color: Colors.purple, fontSize: 25),
+                  )),
+              OutlinedButton(onPressed: () {
+
+              }, child: Text(
+                'Outlined',
+                style: TextStyle(color: Colors.purple, fontSize: 25),
+              ),),
+              FloatingActionButton(onPressed: () {},child: Icon(Icons.add)),
+              TextButton(onPressed: () {
+
+              }, child: Text(
+                'Text',
+                style: TextStyle(color: Colors.purple, fontSize: 25),
+              ),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
